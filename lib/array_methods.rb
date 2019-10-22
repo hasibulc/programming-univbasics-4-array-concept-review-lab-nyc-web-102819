3b95f1aaafdebc7f32f20cc6b669f8e4a89856bb
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-22 21:55:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   if array.include?(value_to_find)
     counter = 0
     
-    while array[counter] != value_to_find
+    while array[counter] != value_to_find do
       counter += 1
     end
     
